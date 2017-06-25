04b70bfd6b0cca166e4c932e051d577b53ae8d1b
mrstebo <mrstebouk@gmail.com>
2017-06-25 22:12:29 +0100

    Added HomeModule and view.

---
+++ PlanetProject.Web/Modules/HomeModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+using PlanetProject.Web.ViewModels;
+
+namespace PlanetProject.Web.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => View["Home"];
+        }
+    }
+}
