affa6621073d6864896591c2a74936c288a8e613
mrstebo <mrstebouk@gmail.com>
2017-06-25 21:47:01 +0100

    Added tests for the Web project.

---
+++ PlanetProject.Web.Tests/Modules/API/PlanetModuleTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using Nancy;
+using Nancy.Testing;
+using NUnit.Framework;
+using PlanetProject.Core.Models;
+using PlanetProject.Core.Services;
+using PlanetProject.Web.Modules.API;
+using PlanetProject.Web.ViewModels;
+
+namespace PlanetProject.Web.Tests.Modules.API
+{
+    [TestFixture]
+    [Parallelizable]
+    public class PlanetModuleTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _planetService = new Mock<IPlanetService>();
+            _browser = new Browser(with =>
+            {
+                with.Module<PlanetModule>();
+                with.Dependency(_planetService.Object);
+            });
+        }
+
+        private Mock<IPlanetService> _planetService;
+        private Browser _browser;
+
+        [Test]
+        public void Get_ShouldReturn_CollectionOf_PlanetViewModel()
+        {
+            _planetService
+                .Setup(x => x.GetAll())
+                .Returns(Enumerable.Range(1, 3).Select(i => new Planet {Name = $"Planet {i}"}));
+
+            var response = _browser.Get("/api/planets", with => with.HttpRequest());
+
+            var result = response.Body.DeserializeJson<List<PlanetViewModel>>();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual("Planet 1", result[0].Name);
+            Assert.AreEqual("Planet 2", result[1].Name);
+            Assert.AreEqual("Planet 3", result[2].Name);
+        }
+
+        [Test]
+        public void Get_WithName_ShouldReturn_PlanetViewModel()
+        {
+            _planetService
+                .Setup(x => x.GetByName("earth"))
+                .Returns(new Planet {Name = "Earth"});
+
+            var response = _browser.Get("/api/planets/earth", with => with.HttpRequest());
+
+            var result = response.Body.DeserializeJson<PlanetViewModel>();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Earth", result.Name);
+        }
+
+        [Test]
+        public void Get_WithName_When_PlanetDoesNotExist_ShouldReturn_NotFound()
+        {
+            _planetService
+                .Setup(x => x.GetByName("pluto"))
+                .Returns(() => null);
+
+            var response = _browser.Get("/api/planets/pluto", with => with.HttpRequest());
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
---
+++ PlanetProject.Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PlanetProject.Web.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PlanetProject.Web.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df0c71d8-7435-4ba8-9912-b864b8d39982")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
